834abb15f4fd65df7dab0052f8720b26fd4ca305
zzz123-tech <66854678+zzz123-tech@users.noreply.github.com>
2020-07-26 15:19:41 +0800

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c4#9)++vwteund6dr560*+outkaq-czam78&#(*bpe010#9_7q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','zz.com']
