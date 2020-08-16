0faafe4f1977b4d42bc9c5b992ab7f42bf3bfe0d
zhanzhen <15951913827@163.com>
2020-08-16 15:22:33 +0800

    updata

--- blog/views.py
+++ blog/views.py
@@ -4,39 +4,43 @@ import markdown
 import re
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from pure_pagination.mixins import PaginationMixin
+from django.views.generic import ListView, DetailView
+class IndexView(PaginationMixin,ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 10
 
-def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
-    return render(request,'blog/index.html',context={'post_list':post_list})
-def detail(request,pk):
-    post = get_object_or_404(Post,pk=pk)
-    post.increase_views()
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        TocExtension(slugify=slugify),
-        ])
-    post.body = md.convert(post.body)
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>',md.toc,re.S)
-    post.toc = m.group(1) if m is not None else ''
-    return render(request,'blog/detail.html',context={'post':post})
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__year=year, created_time__month=month)
+        )
 
-def archive(request,year,month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month,
-                                    ).order_by('-created_time')
-    return render(request,'blog/index.html',context={'post_list':post_list})
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
-def category(request,pk):
-    cate = get_object_or_404(Category,pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time'
-)
-    return render(request,'blog/index.html',context={'post_list':post_list})
+class TagView(IndexView):
+    def get_queryset(self):
+        c = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView,self).get_queryset().filter(tags=c)
 
-def tag(request,pk):
-    c = get_object_or_404(Tag,pk=pk)
-    post_list = Post.objects.filter(tags=c).order_by('-created_time')
-    return render(request,'blog/index.html',context={'post_list':post_list})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
+    context_object_name = "post"
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
 
 
 
--- myblog/settings/common.py
+++ myblog/settings/common.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'comments.apps.CommentsConfig',
+    'pure_pagination',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-
+# django-pure-pagination 分页设置
+PAGINATION_SETTINGS = {
+        'PAGE_RANGE_DISPLAYED': 4, # 分页条当前页前后应该显示的总页数（两边均匀分布，因此要设置为偶数），
+        'MARGIN_PAGES_DISPLAYED': 2, # 分页条开头和结尾显示的页数
+        'SHOW_FIRST_PAGE_WHEN_INVALID': True, # 当请求了不存在页，显示第一页
+        }
 
